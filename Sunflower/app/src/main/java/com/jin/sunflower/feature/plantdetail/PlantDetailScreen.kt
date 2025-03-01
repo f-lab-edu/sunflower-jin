24a96401be03ffe5f1e6dcd687fed6f2482cd80f
YEJIN <ye_s_jin@naver.com>
2025-03-01 23:02:54 +0900

    PlantDetail mock data 정의 및 view 전달

--- Sunflower/app/src/main/java/com/jin/sunflower/feature/plantdetail/PlantDetailScreen.kt
+++ Sunflower/app/src/main/java/com/jin/sunflower/feature/plantdetail/PlantDetailScreen.kt
@@ -1,7 +1,27 @@
 package com.jin.sunflower.feature.plantdetail
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -9,10 +29,56 @@ import com.jin.sunflower.ui.theme.SunflowerTheme
 
 @Composable
 fun PlantDetailScreen(navController: NavController, viewModel: PlantDetailViewModel = viewModel()) {
+    val plantDetail by viewModel.plantDetail.collectAsState()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()) // 세로 스크롤 가능하게 만듦
+            .padding(bottom = 60.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .background(color = Color.Gray),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = plantDetail.imageUrl)
+        }
 
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = plantDetail.name,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        // 물 주어야 하는 주기
+        Text(
+            text = "Watering needs",
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "every ${plantDetail.wateringInterval} days",
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = plantDetail.description,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        )
+
+    }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun PlantDetailScreenPreview() {
     SunflowerTheme {
--- Sunflower/app/src/main/java/com/jin/sunflower/feature/plantdetail/PlantDetailViewModel.kt
+++ Sunflower/app/src/main/java/com/jin/sunflower/feature/plantdetail/PlantDetailViewModel.kt
@@ -1,7 +1,25 @@
 package com.jin.sunflower.feature.plantdetail
 
 import androidx.lifecycle.ViewModel
+import com.jin.sunflower.core.model.Plant
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class PlantDetailViewModel : ViewModel() {
+    private val samplePlantDetail = Plant(
+        id = 1,
+        name = "Apple",
+        description = "An apple is a sweet, edible fruit produced by an apple tree (Malus pumila). Apple trees are cultivated worldwide, and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today. Apples have been grown for thousands of years in Asia and Europe, and were brought to North America by European colonists. Apples have religious and mythological significance in many cultures, including Norse, Greek and European Christian traditions.\n" +
+                "Apple trees are large if grown from seed.\n" +
+                "Generally apple cultivars are propagated by grafting onto rootstocks, which control the size of the resulting tree. There are more than 7,500 known cultivars of apples, resulting in a range of desired\n" +
+                "characteristics. Different cultivars are bred for various tastes and uses, including cooking, eating raw and cider production.\n" +
+                "Trees and fruit are prone to a number of fungal, bacterial and pest problems, which can be controlled by a number of organic and non-organic means. In 2010, the fruit's genome was sequenced as part of research on disease control and selective breeding in apple production.\n" +
+                "Worldwide production of apples in 2014 was 84.6 million tonnes, with China accounting for 48% of the total.",
+        growZoneNumber = 1,
+        wateringInterval = 30,
+        imageUrl = "Apple Image Url"
+    )
 
+    private val _plantDetail = MutableStateFlow(samplePlantDetail)
+    val plantDetail: StateFlow<Plant> = _plantDetail
 }
